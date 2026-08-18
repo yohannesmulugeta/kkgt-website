18e04aa90ce8efd5c4f4053c8601a3d7620e22d1
joni <70434578+yohannesmulugeta@users.noreply.github.com>
2026-08-18 19:49:21 +0300

    Wire full KKGT React route tree

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from 'react-router-dom';
+import { SiteShell } from './components/SiteShell';
+import { About } from './pages/About';
+import { Agrochemicals, ProductDetail } from './pages/Agrochemicals';
+import { Coffee, CoffeeOrigin } from './pages/Coffee';
+import { Commodities, CommodityDetail } from './pages/Commodities';
+import { Contact } from './pages/Contact';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
+import { Quality } from './pages/Quality';
+import { Trading } from './pages/Trading';
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<SiteShell />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="coffee" element={<Coffee />} />
+        <Route path="coffee/:slug" element={<CoffeeOrigin />} />
+        <Route path="commodities" element={<Commodities />} />
+        <Route path="commodities/:slug" element={<CommodityDetail />} />
+        <Route path="agrochemicals" element={<Agrochemicals />} />
+        <Route path="agrochemicals/product/:slug" element={<ProductDetail />} />
+        <Route path="trading" element={<Trading />} />
+        <Route path="quality" element={<Quality />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+}
