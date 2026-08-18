5e9c09d77e24b03df3e50c1c9a3994d38f63b836
joni <70434578+yohannesmulugeta@users.noreply.github.com>
2026-08-18 23:18:12 +0300

    Load homepage cinematic styles last

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import './styles.css';
 import './overrides.css';
 import './site.css';
+import './journey.css';
 
 function ScrollManager() {
   const { pathname } = useLocation();
