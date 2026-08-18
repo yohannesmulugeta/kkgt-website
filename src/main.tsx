4ac6d506ebb909c70c7e44ebaa8cb1cf9a7e43ed
joni <70434578+yohannesmulugeta@users.noreply.github.com>
2026-08-18 19:57:59 +0300

    Load React category styling overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, useLocation } from 'react-router-dom';
 import App from './App';
 import './styles.css';
+import './overrides.css';
 
 function ScrollManager() {
   const { pathname } = useLocation();
